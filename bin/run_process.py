4a5568f92d2d2ae97c0cc3288157e3d792b4278c
astrokatross <kathryn.ross@icrar.org>
2021-05-21 11:54:30 +0800

    Fix sourcepar hardcode :art:

--- bin/run_process.py
+++ bin/run_process.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# This script calls process.py with all functions to analyse ATCA data and executes them in order, only changes needed to script should just be what stepyou need/commenting out whatever you don't need 
+# This script calls process.py with all functions to analyse ATCA data and executes them in order, only changes needed to script should just be what stepyou need/commenting out whatever you don't need
 # TODO: Correct the sourcepar to be less hardcoded
 # By K.Ross 19/5/21
 
@@ -14,9 +14,25 @@ pri = os.environ['PRIMARY_CALIBRATOR']
 sec = os.environ['SECONDARY_CALIBRATOR']
 tar = os.environ['TARGET']
 tar_nm = os.environ['TARGET_NAME']
-sourcepar = [
-    0.1, 12.6, -0.9
-]
+
+# Setting sourcepar dictionary to measrue flux
+sourcepar_dict = {
+    "J001513" : [0.1,12.6,-0.9],
+    "J015445" : [0.15,14.1,-2.7],
+    "J020507" : [0.3,2.6,0.7],
+    "J021246" : [0.15,12.9,-5.2],
+    "J022744" : [0.25,6,0.3],
+    "J024838" : [0.1,1.5,0.5],
+    "J032213" : [0.35,2.6,-3.4],
+    "J032836" : [0.35,4,-2.5],
+    "J033023" : [0.15,0,0],
+    "J042502" : [0.1,4.8,0],
+    "J044033" : [0.2,8.2,-0.3],
+    "J044737" : [0.25,3.4,-2.2],
+    "J052824" : [0.2,8,-3.9],
+    "J223933" : [0.2,10,-3.1],
+    "J224408" : [0.25,8.3,-5.2]
+}
 
 # Defining constant variables for all sources
 src_dir = data_dir + src
@@ -27,6 +43,7 @@ msname = data_dir + "{0}_{1}_{2}.ms".format(epoch, ATCA_band, tar_nm)
 targetms = data_dir + "{0}_{1}_{2}_img.ms".format(epoch, ATCA_band, tar_nm)
 tar_ms = data_dir + "{0}_{1}_{2}_selfcal.ms".format(epoch, ATCA_band, tar_nm)
 ref = "CA04"
+sourcepar = sourcepar_dict[tar_nm]
 if ATCA_band == "L":
     n_spw = 8
     if_centre = 0
@@ -37,7 +54,7 @@ elif ATCA_band == "X":
     n_spw = 4
     if_centre = 1
 
-
+print("Here we go! Time to analyse some ATCA data!")
 # Uncomment whichever step you don't need to do 
 process.flag_ms(visname,epoch,ATCA_band,pri,sec,tar,tar_nm)
 split_ms(visname, msname,  if_centre, epoch, ATCA_band, pri,sec,tar,tar_nm)
